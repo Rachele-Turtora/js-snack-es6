9d819390ff3be63883e5544b62001392c109b434
Rachele-Turtora <racheleturtora21@gmail.com>
2024-05-16 15:54:57 +0200

    [UPD] snack 1

--- snack1/js/main.js
+++ snack1/js/main.js
@@ -1,4 +1,36 @@
 "use strict";
 
+const bikes = [
+    {
+        name: "Galano",
+        weight: "10"
+    },
+    {
+        name: "Hiland",
+        weight: "30"
+    },
+    {
+        name: "Kabon",
+        weight: "5"
+    },
+    {
+        name: "Kootu",
+        weight: "40"
+    }
+]
 
+let weight = parseInt(bikes[0].weight); 
+let bike;
+
+// Comparing bikes
+bikes.forEach((element) => {
+
+    if (parseInt(element.weight) < weight){
+            weight = parseInt(element.weight);
+            bike = element;
+    }
+})
+
+// Printing result
+console.log(`La bici più leggera è: ${bike.name}, che pesa ${bike.weight}`);
 
